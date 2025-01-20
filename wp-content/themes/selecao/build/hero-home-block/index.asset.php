56e9d4f6661bc18d0ec73a559bacaaeb3be4036a
sharad patel <patelsharad7760@gmail.com>
2025-01-20 17:58:11 +0530

    updated hero home

--- wp-content/themes/selecao/build/hero-home-block/index.asset.php
+++ wp-content/themes/selecao/build/hero-home-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4c179cdcbf9e0d4da05f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f03eb535b12ff45e146e');
