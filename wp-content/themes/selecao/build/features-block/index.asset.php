6d965f1b98cda4cfb2496b6075343f1914a63999
sharad patel <patelsharad7760@gmail.com>
2025-01-24 19:16:26 +0530

    updated pricing

--- wp-content/themes/selecao/build/features-block/index.asset.php
+++ wp-content/themes/selecao/build/features-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '23134b260fe0150b6b14');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '149f1287fcdcc4685e61');
