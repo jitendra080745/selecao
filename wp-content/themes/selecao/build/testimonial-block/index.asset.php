f4fb38f89f82bbc3cd829159258a47a098600050
sharad patel <patelsharad7760@gmail.com>
2025-01-24 18:35:55 +0530

    updated testimonial block

--- wp-content/themes/selecao/build/testimonial-block/index.asset.php
+++ wp-content/themes/selecao/build/testimonial-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8a672649d1bff5353955');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7684e6cd53657401e9de');
